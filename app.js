d21b11369fa0fa2fc9eb3f72c4185634c2bcc77b
Umrzoq0323 <umrzoqaraqulov01@gmail.com>
2024-02-19 18:10:48 +0500

    updated

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-let number = document.querySelector("#number");
-let mod = document.querySelector("#mod");
 let submit = document.querySelector(".submit");
 let result = document.querySelector(".result");
+
 submit.addEventListener("click", function (e) {
   e.preventDefault();
-  number = +number.value;
-  mod = +mod.value;
+  let number = +document.querySelector("#number").value;
+  let mod = +document.querySelector("#mod").value;
   let reverseNumber = 2;
   while (true) {
     if ((number * reverseNumber) % mod === 1) {
-      result.textContent+=reverseNumber;
+      result.textContent = "Result : " + reverseNumber;
       break;
     }
     reverseNumber += 1;
     if (reverseNumber > 200) {
-      result.textContent+="not found";
+      result.textContent = "Result : not found";
       break;
     }
   }
